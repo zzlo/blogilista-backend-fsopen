e1446777345452fe83313c33099c333906d122b2
Sami <sami.seisalo@gmail.com>
2021-03-28 20:00:15 +0300

    blogilista - 4.23

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express()
+require('express-async-errors')
 const cors = require('cors')
 const config = require('./utils/config')
 const notesRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
+app.use(middleware.userExtractor)
 
 app.use('/api/blogs', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = process.env.NODE_ENV === 'test' 
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
 module.exports = {
     mongoUrl,
